14a5d984569741237aa664cb13cec80e73cfef22
Nicolás Tinte <nico.tinte@gmail.com>
2020-10-11 15:16:38 -0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,49 @@
+# Upstream trello card creator
+
+This action intention is to create one trello card for each new commit on a given upstream, with the goal of having a better roadmap to have your fork in sync!
+
+## Inputs
+
+#### `TRELLO_API_KEY` (**Required**)
+
+Necessary to use [Trello's API](https://developer.atlassian.com/cloud/trello/rest/). Go to this site to [obtain yours](https://trello.com/app-key) and set it up as a secret in order to use: `${{ secrets.TRELLO_API_KEY }}`.
+
+
+#### `TRELLO_OAUTH_TOKEN` (**Required**)
+
+Necessary to use [Trello's API](https://developer.atlassian.com/cloud/trello/rest/). Go to this site to [obtain yours](https://trello.com/app-key) and set it up as a secret in order to use: `${{ secrets.TRELLO_OAUTH_TOKEN }}`.
+
+
+#### `TRELLO_LIST_ID` (**Required**)
+
+Id of the trello list where the cards will be created. Go to your trello board and add `.json` at the end and search for the id of the desired list and set it up as a secret in order to use: `${{ secrets.TRELLO_LIST_ID }}`.
+
+
+#### `UPSTREAM` (**Required**)
+
+Url of the upstream repository.
+
+## Usage example
+
+```
+name: Create trello card for new upstream commits
+
+on:
+  schedule:
+    - cron: '*/5* * * *' # every 5 minutes
+
+jobs:
+  sync:
+    runs-on: ubuntu-latest
+
+    steps:
+      - uses: tintef/upstream-trello-card-creator@v0.0.1
+        with:
+          TRELLO_API_KEY: ${{ secrets.TRELLO_API_KEY }}
+          TRELLO_OAUTH_TOKEN: ${{ secrets.TRELLO_OAUTH_TOKEN }}
+          TRELLO_LIST_ID: ${{ secrets.TRELLO_LIST_ID }}
+          UPSTREAM: git@github.com:Tintef/upstream-trello-card-creator.git
+```
+
+## Output example
+
---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import TrelloNodeAPI from 'trello-node-api';
+
+async function createCard(commit : string) {
+  const Trello = new TrelloNodeAPI();
+  Trello.setApiKey(core.getInput('TRELLO_API_KEY'));
+  Trello.setOauthToken(core.getInput('TRELLO_OAUTH_TOKEN'));
+
+  const [hash, desc] = commit.trim().split(/\s(.*)/).splice(0,2);
+  await Trello.card.create({
+    name: `${desc} [${hash}]`,
+    desc: `Commit url: ${core.getInput('UPSTREAM')}/commits/${hash}`,
+    pos: 'top',
+    idList: core.getInput('TRELLO_LIST_ID'),
+    due: null,
+    dueComplete: false,
+  });
+}
+
+async function run() {
+  const upstream : string = core.getInput('UPSTREAM');
+  if (!upstream.match(/[^\/]*\.git/gi)) return;
+
+  const upstreamFolder = upstream.match(/[^\/]*\.git/gi)?.[0].replace('.git', '') || '';
+
+  try {
+    // Fetch upstream
+    await exec.exec('git', ['clone', upstream]);
+    process.chdir(`./${upstreamFolder}`);
+    let upstreamLog = '';
+    await exec.exec('git', ['log', '--oneline', '--since="10 mins ago"'], {
+      listeners: {
+        stdout: (data: Buffer) => {
+          upstreamLog += data.toString();
+        },
+      },
+    });
+    const newCommits = upstreamLog.split('\n');
+
+    upstreamLog = '';
+    await exec.exec('git', ['log', '--oneline', '--since="10 mins ago"', '--until="5 mins ago"'], {
+      listeners: {
+        stdout: (data: Buffer) => {
+          upstreamLog += data.toString();
+        },
+      },
+    });
+    const oldCommits = upstreamLog.split('\n');
+
+    process.chdir('..');
+
+    const difference = newCommits.filter((commit) => oldCommits.indexOf(commit) < 0);
+    await Promise.all(difference.map((commit) => createCard(commit)));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+run();
